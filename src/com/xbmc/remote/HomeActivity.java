a9699d1e827a5cffb7fab153234bf16ab414178b
topfs2 <topfs2@xbmc.org>
2009-08-06 22:15:10 +0000

    Starting project from within eclipse.
    git-svn-id: http://android-xbmcremote.googlecode.com/svn/trunk/XBMC Remote@17 677bec2c-7919-11de-bdc7-1f637afd43b1

---
+++ src/com/xbmc/remote/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.xbmc.remote;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
