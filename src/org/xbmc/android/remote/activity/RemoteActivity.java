dffcca715b0765ac064f07e60cd7867361ca89b1
freezy <freezy@xbmc.org>
2009-09-03 10:41:14 +0000

    - added: button animation to remote control
    git-svn-id: http://android-xbmcremote.googlecode.com/svn/trunk/XBMC Remote@37 677bec2c-7919-11de-bdc7-1f637afd43b1

--- src/org/xbmc/android/remote/activity/RemoteActivity.java
+++ src/org/xbmc/android/remote/activity/RemoteActivity.java
@@ -55,126 +55,233 @@ public class RemoteActivity extends Activity {
 	private void setupButtons() {
 
 		// seek back
-		((ImageButton)findViewById(R.id.RemoteXboxImgBtnSeekBack)).setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				vibrate();
-				KeyboardAction("reverse");
+		((ImageButton)findViewById(R.id.RemoteXboxImgBtnSeekBack)).setOnTouchListener(new View.OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+					vibrate();
+					KeyboardAction("reverse");
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_seek_back_down);
+				}
+				if (event.getAction() == MotionEvent.ACTION_UP) {
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_seek_back);
+				}
+				return false;
 			}
 		});
 		// play
-		((ImageButton)findViewById(R.id.RemoteXboxImgBtnPlay)).setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				vibrate();
-				KeyboardAction("p");
+		((ImageButton)findViewById(R.id.RemoteXboxImgBtnPlay)).setOnTouchListener(new View.OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+					vibrate();
+					KeyboardAction("p");
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_play_down);
+				}
+				if (event.getAction() == MotionEvent.ACTION_UP) {
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_play);
+				}
+				return false;
 			}
 		});
 		// seek forward
-		((ImageButton)findViewById(R.id.RemoteXboxImgBtnSeekForward)).setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				vibrate();
-				KeyboardAction("forward");
+		((ImageButton)findViewById(R.id.RemoteXboxImgBtnSeekForward)).setOnTouchListener(new View.OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+					vibrate();
+					KeyboardAction("forward");
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_seek_forward_down);
+				}
+				if (event.getAction() == MotionEvent.ACTION_UP) {
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_seek_forward);
+				}
+				return false;
 			}
 		});
 		
-		
 		// previous
-		((ImageButton)findViewById(R.id.RemoteXboxImgBtnPrevious)).setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				vibrate();
-				mControl.playPrevious();
+		((ImageButton)findViewById(R.id.RemoteXboxImgBtnPrevious)).setOnTouchListener(new View.OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+					vibrate();
+					mControl.playPrevious();
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_previous_down);
+				}
+				if (event.getAction() == MotionEvent.ACTION_UP) {
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_previous);
+				}
+				return false;
 			}
 		});
 		// stop
-		((ImageButton)findViewById(R.id.RemoteXboxImgBtnStop)).setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				vibrate();
-				mControl.stop();
+		((ImageButton)findViewById(R.id.RemoteXboxImgBtnStop)).setOnTouchListener(new View.OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+					vibrate();
+					mControl.stop();
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_stop_down);
+				}
+				if (event.getAction() == MotionEvent.ACTION_UP) {
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_stop);
+				}
+				return false;
 			}
 		});
-		
 		// pause
-		((ImageButton)findViewById(R.id.RemoteXboxImgBtnPause)).setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				vibrate();
-				mControl.pause();
+		((ImageButton)findViewById(R.id.RemoteXboxImgBtnPause)).setOnTouchListener(new View.OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+					vibrate();
+					mControl.pause();
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_pause_down);
+				}
+				if (event.getAction() == MotionEvent.ACTION_UP) {
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_pause);
+				}
+				return false;
 			}
 		});
 		// next
-		((ImageButton)findViewById(R.id.RemoteXboxImgBtnNext)).setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				vibrate();
-				mControl.playNext();
+		((ImageButton)findViewById(R.id.RemoteXboxImgBtnNext)).setOnTouchListener(new View.OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+					vibrate();
+					mControl.playNext();
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_next_down);
+				}
+				if (event.getAction() == MotionEvent.ACTION_UP) {
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_next);
+				}
+				return false;
 			}
 		});
 		
 		// title
-		((ImageButton)findViewById(R.id.RemoteXboxImgBtnTitle)).setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				vibrate();
-				KeyboardAction("title");
+		((ImageButton)findViewById(R.id.RemoteXboxImgBtnTitle)).setOnTouchListener(new View.OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+					vibrate();
+					KeyboardAction("title");
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_title_down);
+				}
+				if (event.getAction() == MotionEvent.ACTION_UP) {
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_title);
+				}
+				return false;
 			}
 		});
 		// up
-		((ImageButton)findViewById(R.id.RemoteXboxImgBtnUp)).setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				vibrate();
-				KeyboardAction("up");
+		((ImageButton)findViewById(R.id.RemoteXboxImgBtnUp)).setOnTouchListener(new View.OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+					vibrate();
+					KeyboardAction("up");
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_up_down);
+				}
+				if (event.getAction() == MotionEvent.ACTION_UP) {
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_up);
+				}
+				return false;
 			}
 		});
 		// info
-		((ImageButton)findViewById(R.id.RemoteXboxImgBtnInfo)).setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				vibrate();
-				KeyboardAction("i");
+		((ImageButton)findViewById(R.id.RemoteXboxImgBtnInfo)).setOnTouchListener(new View.OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+					vibrate();
+					KeyboardAction("i");
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_info_down);
+				}
+				if (event.getAction() == MotionEvent.ACTION_UP) {
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_info);
+				}
+				return false;
 			}
 		});
 		
-		
 		// left
-		((ImageButton)findViewById(R.id.RemoteXboxImgBtnLeft)).setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				vibrate();
-				KeyboardAction("left");
+		((ImageButton)findViewById(R.id.RemoteXboxImgBtnLeft)).setOnTouchListener(new View.OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+					vibrate();
+					KeyboardAction("left");
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_left_down);
+				}
+				if (event.getAction() == MotionEvent.ACTION_UP) {
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_left);
+				}
+				return false;
 			}
 		});
 		// select
-		((ImageButton)findViewById(R.id.RemoteXboxImgBtnSelect)).setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				vibrate();
-				KeyboardAction("enter");
+		((ImageButton)findViewById(R.id.RemoteXboxImgBtnSelect)).setOnTouchListener(new View.OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+					vibrate();
+					KeyboardAction("enter");
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_select_down);
+				}
+				if (event.getAction() == MotionEvent.ACTION_UP) {
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_select);
+				}
+				return false;
 			}
 		});
 		// right
-		((ImageButton)findViewById(R.id.RemoteXboxImgBtnRight)).setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				vibrate();
-				KeyboardAction("right");
+		((ImageButton)findViewById(R.id.RemoteXboxImgBtnRight)).setOnTouchListener(new View.OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+					vibrate();
+					KeyboardAction("right");
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_right_down);
+				}
+				if (event.getAction() == MotionEvent.ACTION_UP) {
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_right);
+				}
+				return false;
 			}
 		});
-
+		
 		// menu
-		((ImageButton)findViewById(R.id.RemoteXboxImgBtnDown)).setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				vibrate();
-				KeyboardAction("down");
+		((ImageButton)findViewById(R.id.RemoteXboxImgBtnMenu)).setOnTouchListener(new View.OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+					vibrate();
+					KeyboardAction("menu");
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_menu_down);
+				}
+				if (event.getAction() == MotionEvent.ACTION_UP) {
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_menu);
+				}
+				return false;
 			}
 		});
 		// down
-		((ImageButton)findViewById(R.id.RemoteXboxImgBtnMenu)).setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				vibrate();
-				KeyboardAction("menu");
+		((ImageButton)findViewById(R.id.RemoteXboxImgBtnDown)).setOnTouchListener(new View.OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+					vibrate();
+					KeyboardAction("down");
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_down_down);
+				}
+				if (event.getAction() == MotionEvent.ACTION_UP) {
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_down);
+				}
+				return false;
 			}
 		});
 		// back 
-		((ImageButton)findViewById(R.id.RemoteXboxImgBtnBack)).setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				vibrate();
-				KeyboardAction("backspace");
+		((ImageButton)findViewById(R.id.RemoteXboxImgBtnBack)).setOnTouchListener(new View.OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+					vibrate();
+					KeyboardAction("backspace");
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_back_down);
+				}
+				if (event.getAction() == MotionEvent.ACTION_UP) {
+					((ImageButton)v).setImageResource(R.drawable.remote_xbox_back);
+				}
+				return false;
 			}
 		});
-		
-		
 		
 /*		
 		final ImageButton PlayPrevButton = (ImageButton) findViewById(R.id.MediaPreviousButton);
