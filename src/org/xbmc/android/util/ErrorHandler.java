d897f61cf9d086aaee863c0768fe4b57d67a478c
freezy <freezy@xbmc.org>
2009-09-04 13:14:27 +0000

    - added: doc to errorhandler
    git-svn-id: http://android-xbmcremote.googlecode.com/svn/trunk/XBMC Remote@58 677bec2c-7919-11de-bdc7-1f637afd43b1

--- src/org/xbmc/android/util/ErrorHandler.java
+++ src/org/xbmc/android/util/ErrorHandler.java
@@ -1,3 +1,24 @@
+/*
+ *      Copyright (C) 2005-2009 Team XBMC
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC Remote; see the file license.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
 package org.xbmc.android.util;
 
 import java.io.IOException;
@@ -14,10 +35,30 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.DialogInterface.OnClickListener;
 
+/**
+ * The idea of the error handler is to have something global which is called
+ * when an exception occurs. Basically it implements a <code>handle()</code>
+ * method which will then further treat the error, depending on the kind of
+ * exception that was thrown.
+ * <br />
+ * For now, we pop up an alert dialog with some message. Since the pop up will
+ * be opened on the page the current activity refers to (and not on the current
+ * page), we'll have to use the <code>setActivity()</code> method in our
+ * activities.
+ * <br />
+ * Anyway this is how it is for now, maybe later we could change it to a less
+ * obtrusive message.
+ * 
+ * @author Team XBMC
+ */
 public class ErrorHandler implements IErrorHandler {
 	
 	private static Activity sActivity;
 	
+	/**
+	 * An activity is needed for the (potential) GUI stuff.
+	 * @param activity
+	 */
 	public ErrorHandler(Activity activity) {
 		sActivity = activity;
 	}
@@ -26,6 +67,10 @@ public class ErrorHandler implements IErrorHandler {
 		sActivity = activity;
 	}
 	
+	/**
+	 * Handles an exception.
+	 * @param exception Exception to handle.
+	 */
 	public void handle(Exception exception) {
 		final AlertDialog.Builder builder = new AlertDialog.Builder(sActivity);
 		try {
