5ed918f42dad1fc647036095b1874f9cced27baa
freezy <freezy@xbmc.org>
2009-09-04 09:21:53 +0000

    - added: some more doc
    git-svn-id: http://android-xbmcremote.googlecode.com/svn/trunk/XBMC Remote@48 677bec2c-7919-11de-bdc7-1f637afd43b1

--- src/org/xbmc/httpapi/HttpClient.java
+++ src/org/xbmc/httpapi/HttpClient.java
@@ -21,58 +21,104 @@
 
 package org.xbmc.httpapi;
 
-import java.util.ArrayList;
-import java.util.PriorityQueue;
-
 import org.xbmc.httpapi.client.ControlClient;
 import org.xbmc.httpapi.client.InfoClient;
 import org.xbmc.httpapi.client.MusicClient;
 import org.xbmc.httpapi.client.VideoClient;
-import org.xbmc.httpapi.type.DirectoryMask;
-import org.xbmc.httpapi.type.MediaType;
 
-import android.app.Activity;
-import android.content.Context;
 
 /**
- * Wrapper class for our HTTP clients
+ * Wrapper class for our HTTP clients. The idea is to separate the loads of
+ * API method we're going to have into separate classes. The HttpClient class
+ * instantiates them and keeps them in a central place, typically accessible by
+ * the ConnectionManager. 
+ * <p>
+ * So in your code, all you have to do to access the API is getting a HttpClient
+ * instance and access the clients:
+ * <p>
+ * <pre>
+ *  HttpClient client = ConnectionManager.getHttpClient(this);
+ *  String xbmcVersion = client.info.getSystemInfo(SystemInfo.SYSTEM_BUILD_VERSION);
+ *  ArrayList<Album> albums = client.music.getAlbums()</pre>
+ *   
+ * Since the ConnectionManager keeps a static instance of the HttpClient, you
+ * can also get the client without passing an activity once it has already been
+ * used:
+ * <p>
+ * <pre>
+ *  HttpClient client = ConnectionManager.getHttpClient();
+ *</pre>
  * 
  * @author Team XBMC
  */
 public class HttpClient {
 	
+	/**
+	 * Use this client for anything system related
+	 */
 	public final InfoClient info;
+	
+	/**
+	 * Use this client for anything music related
+	 */
 	public final MusicClient music;
+	
+	/**
+	 * Use this client for anything video related
+	 */
 	public final VideoClient video;
+	
+	/**
+	 * Use this client for anything media controller related
+	 */
 	public final ControlClient control;
 	
+
 	private final Connection mConnection;
 
+	/**
+	 * Construct with host only
+	 * @param host         Host or IP address to XBMC
+	 * @param errorHandler Error handler
+	 */
 	public HttpClient(String host, IErrorHandler errorHandler) {
 		this(host, -1, null, null, errorHandler);
 	}
 	
+	/**
+	 * Construct with host and custom port
+	 * @param host         Host or IP address to XBMC
+	 * @param port         Port to the webserver
+	 * @param errorHandler Error handler
+	 */
 	public HttpClient(String host, int port, IErrorHandler errorHandler) {
 		this(host, port, null, null, errorHandler);
 	}
 
+	/**
+	 * Construct with additional login credentials
+	 * @param host         Host or IP address to XBMC
+	 * @param username     Username
+	 * @param password     Password
+	 * @param errorHandler Error handler
+	 */
 	public HttpClient(String host, String username, String password, IErrorHandler errorHandler) {
 		this(host, -1, username, password, errorHandler);
 	}
 
+	/**
+	 * Construct with all paramaters
+	 * @param host         Host or IP address to XBMC
+	 * @param port         Port to XBMC's webserver
+	 * @param username     Password
+	 * @param password     Username
+	 * @param errorHandler Error handler
+	 */
 	public HttpClient(String host, int port, String username, String password, IErrorHandler errorHandler) {		
 		mConnection= new Connection(host, port, username, password, errorHandler);
 		info = new InfoClient(mConnection);
 		music = new MusicClient(mConnection);
 		video = new VideoClient(mConnection);
 		control = new ControlClient(mConnection);
-	}
-
-	public PriorityQueue<Message> getMessenger() {
-		return null;//messenger;
-	}
-	
-	public boolean hasMessages() {
-		return false;// !messenger.isEmpty();
 	}
 }
