53bb7688034a22738c83747e628f9bb8762dd644
freezy <freezy@xbmc.org>
2009-09-04 13:01:30 +0000

    - added: doc to settings activity
    git-svn-id: http://android-xbmcremote.googlecode.com/svn/trunk/XBMC Remote@57 677bec2c-7919-11de-bdc7-1f637afd43b1

--- src/org/xbmc/android/remote/activity/SettingsActivity.java
+++ src/org/xbmc/android/remote/activity/SettingsActivity.java
@@ -1,20 +1,47 @@
+/*
+ *      Copyright (C) 2005-2009 Team XBMC
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC Remote; see the file license.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
 package org.xbmc.android.remote.activity;
 
 import java.util.Hashtable;
-import java.util.prefs.Preferences;
+
+import org.xbmc.android.remote.R;
 
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
 
-import org.xbmc.android.remote.R;
-import org.xbmc.android.util.ErrorHandler;
-
+/**
+ * The XBMC remote's preferences page. This is a little special since we want
+ * the actual value of the setting in the summary text. It can be set using the
+ * %value% place holder.
+ * <p>
+ * For that we needed to cache the original (with %value% intact) summaries,
+ * which then will be replaced upon change or application start. 
+ * 
+ * @author Team XBMC
+ */
 public class SettingsActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
 	
 	public final static String SUMMARY_VALUE_PLACEHOLDER = "%value%";
@@ -40,6 +67,9 @@ public class SettingsActivity extends PreferenceActivity implements OnSharedPref
 		updateSummaries();
 	}
 	
+	/**
+	 * Updates summaries of all known keys with the updated value.
+	 */
 	private void updateSummaries() {
 		PreferenceScreen ps = getPreferenceScreen();
 		for (String key : ps.getSharedPreferences().getAll().keySet()) {
