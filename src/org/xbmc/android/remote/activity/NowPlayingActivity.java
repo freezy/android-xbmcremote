bb6f89bdfda9e20a4071048d7d4ff6da5b0a4532
topfs2 <topfs2@xbmc.org>
2009-09-05 13:46:38 +0000

    Moved all data gathering in nowplaying to a seperate thread which posts back to the activity with new data, now is the activity much snappier.
    Also this merges in the download thread in the data gathering thread, I've leaved the download thread code though as it may be usefull.
    
    git-svn-id: http://android-xbmcremote.googlecode.com/svn/trunk/XBMC Remote@74 677bec2c-7919-11de-bdc7-1f637afd43b1

--- src/org/xbmc/android/remote/activity/NowPlayingActivity.java
+++ src/org/xbmc/android/remote/activity/NowPlayingActivity.java
@@ -21,9 +21,16 @@
 
 package org.xbmc.android.remote.activity;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Serializable;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
+
 import org.xbmc.android.remote.R;
+import org.xbmc.android.util.Base64;
 import org.xbmc.android.util.ConnectionManager;
 import org.xbmc.android.util.DownloadCallback;
 import org.xbmc.android.util.DownloadThread;
@@ -36,6 +43,8 @@ import org.xbmc.httpapi.type.SeekType;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -50,14 +59,13 @@ import android.widget.TextView;
 import android.widget.ImageView.ScaleType;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 
-public class NowPlayingActivity extends Activity implements Callback, DownloadCallback {
+public class NowPlayingActivity extends Activity implements Callback, Runnable {
 	private ControlClient control;
 	private InfoClient info;
 	private Handler nowPlayingHandler;
 	private String lastPos = "-1";
-	private Bitmap mCover;
 	private String mCoverPath;
-	private boolean isPlaying;
+	private Drawable mCover;
 	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -76,39 +84,6 @@ public class NowPlayingActivity extends Activity implements Callback, DownloadCa
   	  	nowPlayingHandler = new Handler(this);
   	  	setupButtons();
   	  	nowPlayingHandler.sendEmptyMessage(1);
-	}
-
-	private void setupPlayingInfo() {
-		final TextView artist = (TextView) findViewById(R.id.ArtistTextView);
-  	  	artist.setText(info.getMusicInfo(MusicInfo.MUSICPLAYER_ARTIST));
-  	  	
-  	  	final TextView album = (TextView) findViewById(R.id.AlbumTextView);
-  	  	album.setText(info.getMusicInfo(MusicInfo.MUSICPLAYER_ALBUM));
-  	  	
-  	  	final TextView song = (TextView) findViewById(R.id.SongTextView);
-  	  	song.setText(info.getMusicInfo(MusicInfo.MUSICPLAYER_TITLE));
-  	  	
-		try {
-			String downloadURI = info.getCurrentlyPlayingThumbURI();
-			final ImageView cover = (ImageView) findViewById(R.id.CoverImage);
-
-			if (downloadURI != null && downloadURI.length() > 0) {
-				if (!downloadURI.equals(mCoverPath)) {
-		  	  		cover.setImageResource(R.drawable.cover_downloading);
-		  	  		mCoverPath = downloadURI;
-			  	  	Thread downloadThread = new DownloadThread(new String[] { downloadURI }, this);
-			  	  	downloadThread.start();
-				}
-			} else {
-	  	  		cover.setImageResource(R.drawable.nocover);
-			}
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 
 	private void setupButtons() {
@@ -132,17 +107,14 @@ public class NowPlayingActivity extends Activity implements Callback, DownloadCa
 		PlayPrevButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				if (control.playPrevious())
-					updatePlayingInfo();
+					nowPlayingHandler.sendEmptyMessage(1);
 			}
 		});
 		final ImageButton PlayButton = (ImageButton) findViewById(R.id.MediaPlayPauseButton);
 		PlayButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				if (control.pause()) {
-					isPlaying = !isPlaying;
-					final ImageButton PlayPauseButton = (ImageButton) findViewById(R.id.MediaPlayPauseButton);
-					PlayPauseButton.setImageResource(isPlaying ? android.R.drawable.ic_media_pause : android.R.drawable.ic_media_play);
-				}
+				if (control.pause())
+					nowPlayingHandler.sendEmptyMessage(1);
 			}
 		});
 		
@@ -150,58 +122,132 @@ public class NowPlayingActivity extends Activity implements Callback, DownloadCa
 		PlayNextButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				if (control.playNext())
-					updatePlayingInfo();
+					nowPlayingHandler.sendEmptyMessage(1);
 			}
 		});
 	}
 
+	Thread updateViewThread;
+	
 	public boolean handleMessage(Message msg) {
 		if (msg.what == 1) {
-			updatePlayingInfo();
+			if (updateViewThread == null || !updateViewThread.isAlive()) {
+				updateViewThread = new Thread(this);
+				updateViewThread.start();
+			}
 			nowPlayingHandler.sendEmptyMessageDelayed(1, 1000);
 			return true;
-		} else if (msg.what == 2) {
-			setupPlayingInfo();
+		} else if (msg.what == 666) {
+			Bundle data = msg.getData();
+			final SeekBar seekBar = (SeekBar) findViewById(R.id.NowPlayingProgress);
+			seekBar.setProgress(data.getInt("progress"));
+			
+			final ImageButton PlayPauseButton = (ImageButton) findViewById(R.id.MediaPlayPauseButton);
+			boolean isPlaying = data.getBoolean("isplaying");
+			PlayPauseButton.setImageResource(isPlaying ? android.R.drawable.ic_media_pause : android.R.drawable.ic_media_play);
 			return true;
-		} else if (msg.what == 3) {
-			if (mCover != null) {
-	  	  		final ImageView cover = (ImageView) findViewById(R.id.CoverImage);
-	  	  		cover.setImageBitmap(mCover);
-			}
+		} else if (msg.what == 667) {
+			Bundle data = msg.getData();
+			final TextView artist = (TextView) findViewById(R.id.ArtistTextView);
+			String sa =data.getString("artist"); 
+	  	  	artist.setText(data.getString("artist"));
+	  	  	
+	  	  	final TextView album = (TextView) findViewById(R.id.AlbumTextView);
+	  	  	String sal = data.getString("album");
+	  	  	album.setText(data.getString("album"));
+	  	  	
+	  	  	final TextView song = (TextView) findViewById(R.id.SongTextView);
+	  	  	String st = data.getString("title");
+	  	  	song.setText(data.getString("title"));
+	  	  	return true;
+		} else if (msg.what == 668) {
+			final ImageView cover = (ImageView) findViewById(R.id.CoverImage);
+			cover.setImageDrawable(mCover);
 			return true;
 		}
 		return false;
 	}
-	
-	private void updatePlayingInfo() {
-		final SeekBar seekBar = (SeekBar) findViewById(R.id.NowPlayingProgress);
-		int progress = control.getPercentage();
-		seekBar.setProgress(progress);
+
+	public void run() {
+		Message msg = new Message();
+		Bundle bundle = msg.getData();
+		msg.what = 666;
+		
+		bundle.putInt("progress", control.getPercentage());
 		
 		CurrentlyPlaying currPlaying = info.getCurrentlyPlaying();
-		
-		isPlaying = currPlaying != null && currPlaying.isPlaying;
-		final ImageButton PlayPauseButton = (ImageButton) findViewById(R.id.MediaPlayPauseButton);
-		PlayPauseButton.setImageResource(isPlaying ? android.R.drawable.ic_media_pause : android.R.drawable.ic_media_play);
+		bundle.putBoolean("isplaying", currPlaying != null && currPlaying.isPlaying);
 		
 		String currentPos = info.getMusicInfo(MusicInfo.MUSICPLAYER_TITLE) + info.getMusicInfo(MusicInfo.MUSICPLAYER_DURATION);
-
+		nowPlayingHandler.sendMessage(msg);
+		
 		if (!lastPos.equals(currentPos)) {
-			nowPlayingHandler.sendEmptyMessage(2);
 			lastPos = currentPos;
+			msg = new Message();
+			bundle = msg.getData();
+	  	  	msg.what = 667;
+			
+	  	  	String album = info.getMusicInfo(MusicInfo.MUSICPLAYER_ALBUM);
+	  	  	String artist = info.getMusicInfo(MusicInfo.MUSICPLAYER_ARTIST);
+	  	  	String title = info.getMusicInfo(MusicInfo.MUSICPLAYER_TITLE);
+	  	  	
+			bundle.putString("album", album);
+			bundle.putString("artist", artist);
+			bundle.putString("title", title);
+			
+			nowPlayingHandler.sendMessage(msg);
+			
+			try {				
+				String downloadURI = info.getCurrentlyPlayingThumbURI();
+
+				if (downloadURI != null && downloadURI.length() > 0) {
+					if (!downloadURI.equals(mCoverPath)) {
+			  	  		mCover = this.getResources().getDrawable(R.drawable.cover_downloading);
+			  	  		mCoverPath = downloadURI;
+			  	  		nowPlayingHandler.sendEmptyMessage(668);
+			  	  		
+				  	  	/*Thread downloadThread = new DownloadThread(new String[] { downloadURI }, this);
+				  	  	downloadThread.start();*/
+			  	  		
+			  	  		byte[] buffer = download(downloadURI);
+
+			  	  		if (buffer != null) {
+					  	  	mCover = new BitmapDrawable(BitmapFactory.decodeByteArray(buffer, 0, buffer.length));
+					  	  	nowPlayingHandler.sendEmptyMessage(668);
+			  	  		}
+					}
+				} else {
+					mCover = this.getResources().getDrawable(R.drawable.nocover);
+					mCoverPath = null;
+					nowPlayingHandler.sendEmptyMessage(668);
+				}
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 
-	public void onDownloadDone(byte[] buffer) {
-		if (buffer == null)
-			return;
-		
+	private byte[] download(String pathToDownload) {
 		try {
-			mCover = BitmapFactory.decodeByteArray(buffer, 0, buffer.length);
-			nowPlayingHandler.sendEmptyMessage(3);
+			final URL url = new URL(pathToDownload);
+			final URLConnection uc = url.openConnection();
+			
+			final BufferedReader rd = new BufferedReader(new InputStreamReader(uc.getInputStream()), 8192);
+			
+			final StringBuilder sb = new StringBuilder();
+			String line = "";
+			while ((line = rd.readLine()) != null) {    
+				sb.append(line);
+			}
+			
+			rd.close();
+			return Base64.decode(sb.toString().replace("<html>", "").replace("</html>", ""));
 		} catch (Exception e) {
-			System.err.println("ERROR: " + e.getMessage());
-			e.printStackTrace();
+			return null;
 		}
 	}
 }
