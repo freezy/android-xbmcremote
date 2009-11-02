6311c8eb5e9fab99a901411a758985bdb9caa0b3
freezy <freezy@xbmc.org>
2009-11-02 09:24:33 +0000

    Added: GPL headers
    git-svn-id: http://android-xbmcremote.googlecode.com/svn/trunk/XBMC Remote@232 677bec2c-7919-11de-bdc7-1f637afd43b1

--- src/org/xbmc/android/remote/guilogic/ListLogicOnKeyListener.java
+++ src/org/xbmc/android/remote/guilogic/ListLogicOnKeyListener.java
@@ -1,3 +1,24 @@
+/*
+ *      Copyright (C) 2005-2009 Team XBMC
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC Remote; see the file license.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
 package org.xbmc.android.remote.guilogic;
 
 import org.xbmc.httpapi.data.NamedResource;
@@ -9,9 +30,7 @@ import android.widget.ListView;
 
 public class ListLogicOnKeyListener<T extends NamedResource> implements OnKeyListener {
 
-	public ListLogicOnKeyListener(){
-		
-	}
+	public ListLogicOnKeyListener(){ }
 	
 	@SuppressWarnings("unchecked")
 	public boolean onKey(View v, int keyCode, KeyEvent event) {
@@ -43,6 +62,4 @@ public class ListLogicOnKeyListener<T extends NamedResource> implements OnKeyLis
 		}
 		return false; //event is NOT eaten
 	}
-	
-
 }
