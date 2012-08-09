4000f40d153fb9fc0502fdec4959b6160e516739
Tom <tom@decompile.me.uk>
2012-08-09 19:43:14 +0100

    FileList view: made click and play context menu use the playlist so other things can be queued after

--- src/org/xbmc/android/remote/presentation/activity/EpisodeDetailsActivity.java
+++ src/org/xbmc/android/remote/presentation/activity/EpisodeDetailsActivity.java
@@ -158,7 +158,7 @@ public class EpisodeDetailsActivity extends Activity {
 			button.setOnClickListener(new OnClickListener() {
 				public void onClick(View v) {
 					Toast.makeText(mActivity, "Playing Episode", Toast.LENGTH_LONG).show();
-					mControlManager.clearPlaylist(new DataResponse<Boolean>(), "", mActivity);
+					mControlManager.clearPlaylist(new DataResponse<Boolean>(), "1", mActivity);
 					mControlManager.setPlaylistId(new DataResponse<Boolean>(), 1, mActivity);
 					mControlManager.addToPlaylist(new DataResponse<Boolean>(), mEpisode.getPath(), mActivity);					
 					mControlManager.setPlaylistPos(new DataResponse<Boolean>(){
@@ -167,7 +167,7 @@ public class EpisodeDetailsActivity extends Activity {
 							mActivity.startActivity(new Intent(mActivity, NowPlayingActivity.class));
 							}
 						}
-					}, 1, mActivity.getApplicationContext());
+					}, 0, mActivity.getApplicationContext());
 				}
 			});
 		}
